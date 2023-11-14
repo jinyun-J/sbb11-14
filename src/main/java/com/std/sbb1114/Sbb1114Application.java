daf6b8d3c0bb2e737be9e2b1e518c158986bad4c
ㅓjinyun-J <ppp6289@gmail.com>
2023-11-14 12:22:40 +0900

    first commit

---
+++ src/main/java/com/std/sbb1114/Sbb1114Application.java
@@ -0,0 +1,13 @@
+package com.std.sbb1114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb1114Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbb1114Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/std/sbb1114/Sbb1114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.std.sbb1114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb1114ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
