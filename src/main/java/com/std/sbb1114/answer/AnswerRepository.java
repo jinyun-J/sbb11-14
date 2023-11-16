a0ab47559cf02863b854db6d93891bfbee479081
ㅓjinyun-J <ppp6289@gmail.com>
2023-11-16 12:39:32 +0900

    2

---
+++ src/main/java/com/std/sbb1114/answer/AnswerRepository.java
@@ -0,0 +1,8 @@
+package com.std.sbb1114.answer;
+
+import com.std.sbb1114.answer.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
